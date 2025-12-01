d37cd00c7986d830dbec3140329db5bd83f64f55
HafizSyedAhmedAli <hafizsyedahmedali12@gmail.com>
2025-12-01 18:14:26 +0500

    Fixed caching issues

--- app/events/create/page.tsx
+++ app/events/create/page.tsx
@@ -1,8 +1,9 @@
 // app/events/create/page.tsx
-import { redirect } from "next/navigation";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import {redirect} from "next/navigation";
+import {getServerSession} from "next-auth/next";
+import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 import CreateEventForm from "@/components/CreateEventForm";
+import {Suspense} from "react";
 
 const CreateEventPage = async () => {
     const session = await getServerSession(authOptions);
@@ -12,7 +13,12 @@ const CreateEventPage = async () => {
         redirect("/api/auth/signin");
     }
 
-    return <CreateEventForm />;
-};
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <CreateEventForm/>
+            );
+        </Suspense>
+    );
+}
 
 export default CreateEventPage;
