cf1928e11cdb933a470527c139e796d1bef36d6b
HafizSyedAhmedAli <hafizsyedahmedali12@gmail.com>
2025-12-01 18:38:50 +0500

    Fixed error with Posthog

--- app/events/create/page.tsx
+++ app/events/create/page.tsx
@@ -3,7 +3,6 @@ import {redirect} from "next/navigation";
 import {getServerSession} from "next-auth/next";
 import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 import CreateEventForm from "@/components/CreateEventForm";
-import {Suspense} from "react";
 
 const CreateEventPage = async () => {
     const session = await getServerSession(authOptions);
@@ -14,10 +13,7 @@ const CreateEventPage = async () => {
     }
 
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <CreateEventForm/>
-            );
-        </Suspense>
+        <CreateEventForm/>
     );
 }
 
